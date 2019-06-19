3b8963111c9ea1a757bc7da3fb3ec1cf2c79fe2a
Juan Hernando <juanantonio.hernandolabajo@telefonica.com>
2019-06-19 20:03:10 +0200

    fix: change gpio levels

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/labajo/OctoPrint-Ledblinker"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = ["RPi.GPIO>=0.6.5"]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
